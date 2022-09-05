2105c3e1bfd8ca3be95b3b63610d448d83757cba
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-09-05 13:33:28 +0200

    opgave 2

---
+++ ClassRoomNet60/Klasserum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class Klasserum
+    {
+        private string _klassenavn;
+        private List<Studerende> _klasseliste;
+        private DateTime _semesterstart;
+
+        public string Klassenavn
+        {
+            get { return _klassenavn; }
+            set { _klassenavn = value; }
+        }
+
+        public List<Studerende> Klasseliste
+        {
+            get { return _klasseliste; }
+            set { _klasseliste = value; }
+        }
+
+        public DateTime Semesterstart
+        {
+            get { return _semesterstart; }
+            set { _semesterstart = value; }
+        }
+
+        public Klasserum()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"Klassenavn = {_klassenavn}, Klasselist = {_klasseliste}, Semesterstart = {_semesterstart}";
+        }
+    }
+}
